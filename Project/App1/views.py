7a64a3cfd3d765a2c73f8d07767fc0528024622c
skrishnarjuna <s.krishnarjuna@gmail.com>
2022-08-28 18:43:23 +0530

    first commit

--- Project/App1/views.py
+++ Project/App1/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render
 class college:
     def app1view(request):
         return render(request, 'App1/app1view.html', {})
+class student:
+    def app1view(request):
+        return render(request, 'App1/app1view.html', {})
